05690b53b9b0da2429e84b75eb7c1a22d38aa731
abkalkaled <aminukhaleedabiola@gmail.com>
2023-12-20 14:10:32 -0800

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class square"""
+
+
+class Square:
+    """class square for sqaure"""
+
+    def __init__(self, size):
+        """Initilialize square"""
+
+        self_size = size
