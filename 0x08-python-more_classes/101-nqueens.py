e808bc2ecc4fea61c90070b90a8a8fab91a6b32b
abkalkaled <aminukhaleedabiola@gmail.com>
2024-01-04 13:54:21 -0800

    nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 def is_safe(board, row, col, N):
     """
 
@@ -15,11 +16,13 @@ def is_safe(board, row, col, N):
             return False
     return True
 
+
 def print_solution(board, N):
     for i in range(N):
         line = ["." if j != board[i] else "Q" for j in range(N)]
         print("".join(line))
     print()
+
 
 def solve_nqueens(board, row, N):
     if row == N:
@@ -32,6 +35,7 @@ def solve_nqueens(board, row, N):
             board[row] = col
             solve_nqueens(board, row + 1, N)
 
+
 def nqueens(N):
     if not N.isdigit():
         print("N must be a number")
@@ -42,6 +46,7 @@ def nqueens(N):
         print("N must be at least 4")
         sys.exit(1)
 
+
     board = [-1] * N
     solve_nqueens(board, 0, N)
 
@@ -49,6 +54,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
+
 
     nqueens(sys.argv[1])
 
