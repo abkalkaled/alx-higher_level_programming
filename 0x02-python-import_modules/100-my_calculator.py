b9b837912e9819814a8b9263aa0b5bfbaddf107f
abkalkaled <aminukhaleedabiola@gmail.com>
2024-01-06 00:20:06 -0800

    24

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    def calculate(a, operator, b)
+    if operator == " + ":
+        return a + b
+    elif operator = " - ":
+        return a - b
+    elif operator = " * ":
+        return a * b
+    elif operator = " / ":
+        return a / b
+    else:
