55773244afdafb6bbeacf6576509d9676d72bf2c
abkalkaled <aminukhaleedabiola@gmail.com>
2024-01-08 13:48:46 -0800

    inherit from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    4-inherits_from: inherits_from()
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+        inherits_from returns true if object is instance
+        Args:
+            obj (object): object.
+            a_class (class): class.
+        Returns: True or False.
+    """
+    if not type(obj) is a_class and issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
