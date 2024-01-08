b04c8b959238814d212ea897db7d78f187659d37
abkalkaled <aminukhaleedabiola@gmail.com>
2024-01-08 12:06:39 -0800

    lookup mate

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    0-lookup: lookup()
+"""
+
+
+def lookup(obj):
+    """
+        Returns the lista
+        Args:
+            obj (object): object.
+    """
+    return (dir(obj))
