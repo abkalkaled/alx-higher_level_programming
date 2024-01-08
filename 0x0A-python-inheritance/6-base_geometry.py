2a49a2c3540906a87b07674733e3df142b1d0bcc
abkalkaled <aminukhaleedabiola@gmail.com>
2024-01-08 14:06:59 -0800

    improve Geometry trial

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    5-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+        BaseGeometry
+        Attributes: None.
+        Methods:
+            area() - raises an Exceptioni
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
