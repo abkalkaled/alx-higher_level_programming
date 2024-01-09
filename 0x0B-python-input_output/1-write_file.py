f83b7d28eb798fe51763e6d1d820816fe7b93703
abkalkaled <aminukhaleedabiola@gmail.com>
2024-01-09 12:19:59 -0800

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,7 @@
 
 def write_file(filename="", text=""):
     """
-        function that writes a string of textfile and returnd number of character
+        function that writes a string of textfile and returnd num of charcter
     """
     with open(filename, "w", encoding='utf-8') as text_file:
         return text_file.write(text)
