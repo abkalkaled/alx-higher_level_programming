454ee622f858928818cecc5a2fb09662e240ed13
abkalkaled <aminukhaleedabiola@gmail.com>
2024-01-09 12:14:09 -0800

    write a string

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    writes string to text file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+        function that writes a string of textfile and returnd number of character
+    """
+    with open(filename, "w", encoding='utf-8') as text_file:
+        return text_file.write(text)
