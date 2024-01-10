6cc0b2977c5c21c6171afe4273c99c7ccc6cef73
abkalkaled <aminukhaleedabiola@gmail.com>
2024-01-10 02:58:42 -0800

    json encrpt

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+    json_string(my_obj)
+"""
+
+
+def to_json_string(my_obj):
+    """
+        function that returns JSON encryption of an object
+    """
+    return json.dumps(my_obj)
