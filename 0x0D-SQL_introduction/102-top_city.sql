4bbeab09ce2949fc277048667c788084beda6f90
abkalkaled <aminukhaleedabiola@gmail.com>
2024-02-15 12:19:06 -0800

    month in int

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Script that display top 3 cities temperature
 SELECT city, AVG(value) AS avg_temp
 FROM temperature
-WHERE month BETWEEN 7 AND 8
+WHERE month = 7 AND month = 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
