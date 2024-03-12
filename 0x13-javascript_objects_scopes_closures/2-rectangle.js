9af06ce9676ec1eb6528091636efd4b58d7e8cd9
abkalkaled <aminukhaleedabiola@gmail.com>
2024-03-12 14:08:56 -0700

    2-rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * Rectangle with instance attribute
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
