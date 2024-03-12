6e8f58e51a225e12df8f71597d5f7a8e9413ad83
abkalkaled <aminukhaleedabiola@gmail.com>
2024-03-12 14:57:48 -0700

    print recatnagle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/**
+ * Rectangle with instance attribute
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
+    }
+      this.width = w;
+      this.height = h;
+    }
+}
+
+print () {
+  for (let i = 0; i < this.height; i++) {
+    console.log ('X'.repeat(this.width));
+  }
+  }
+
+module.exports = Rectangle;
