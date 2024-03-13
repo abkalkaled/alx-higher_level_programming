979cf0c377546c0d40690de87fe2feb422121a58
abkalkaled <aminukhaleedabiola@gmail.com>
2024-03-13 14:05:06 -0700

    multi languages with for loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const programMssg = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < programMssg.Length; i++) {
+  console.log(programMssg[i]);
+}
