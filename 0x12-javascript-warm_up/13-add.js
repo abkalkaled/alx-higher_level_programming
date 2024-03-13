0ed8feed32f903fd1beee98a3f571a69b11140ee
abkalkaled <aminukhaleedabiola@gmail.com>
2024-03-13 14:30:31 -0700

    call

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-function add (a, b) {
-  return a + b;
-}
-
-console.log(
-  add(Number.parseInt(process.argv[2]), Number.parseInt(process.argv[3]))
-);
+module.exports = {
+  add: (a, b) => a + b
+};
