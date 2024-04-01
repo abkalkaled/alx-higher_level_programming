baae98b3760469294a3cbac3c6c2735c9ef6b22b
abkalkaled <aminukhaleedabiola@gmail.com>
2024-04-01 14:42:14 -0700

    git repo commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""10 recent commits in repo"""
+"""lists the 10 most recent commits on repo"""
 import sys
 import requests
 
+
 if __name__ == "__main__":
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        repo_info = response.json()
-        print("Repository name:", repo_info["name"])
-        print("Owner:", repo_info["owner"]["login"])
-        print("Description:", repo_info["description"])
-        print("URL:", repo_info["html_url"])
-        print("Language:", repo_info["language"])
-        print("Stars:", repo_info["stargazers_count"])
-    else:
-        print("Failed to retrieve. Status code:", response.status_code)
+    repo = requests.get(url)
+    commits = repo.json()
+    try:
+        for r in range(10):
+            print("{}: {}".format(
+                commits[r].get("sha"),
+                commits[r].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
